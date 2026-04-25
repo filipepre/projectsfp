6574d31430f6056e0b4fa46437b9dff56f2d6258
filipepre <49457814+filipepre@users.noreply.github.com>
2026-04-25 22:01:33 +0100

    2.1.5 Fix paths

---
+++ components/navbar/script.js
@@ -0,0 +1,5 @@
+const BASE_URL = location.hostname === "filipepre.github.io" ? "/projectsfp" : "";
+
+document.getElementById("home").href = BASE_URL + "/index.html";
+document.getElementById("projects").href = BASE_URL + "/pages/projects/index.html";
+document.getElementById("information").href = BASE_URL + "/pages/information/index.html";
