03fb795c772239367769d1885d539da3d0a7e178
filipepre <49457814+filipepre@users.noreply.github.com>
2026-04-25 21:52:06 +0100

    2.1.4 Fix paths

--- pages/projects/script.js
+++ pages/projects/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const components = [
-    { id: "header", path: "components/header/index.html", js: "components/header/script.js", css: "components/header/style.css" },
-    { id: "navbar", path: "components/navbar/index.html", js: "components/navbar/script.js", css: "components/navbar/style.css" },
-    { id: "dropbox", path: "components/dropbox/index.html", js: "components/dropbox/script.js", css: "components/dropbox/style.css" }
+    { id: "header", path: "/components/header/index.html", js: "/components/header/script.js", css: "/components/header/style.css" },
+    { id: "navbar", path: "/components/navbar/index.html", js: "/components/navbar/script.js", css: "/components/navbar/style.css" },
+    { id: "dropbox", path: "/components/dropbox/index.html", js: "/components/dropbox/script.js", css: "/components/dropbox/style.css" }
   ];
 
   for (const c of components) {
